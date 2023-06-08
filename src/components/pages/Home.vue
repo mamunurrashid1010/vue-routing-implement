211401a76f950e00ba44c9cc171a097f25ec198f
mamunurrashid1010 <mamunurrashid1010@gmail.com>
2023-06-08 11:07:48 +0600

    Redirect : redirect example added.

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <h1>Home Page</h1>
+    <p>Redirect demo</p>
+    <button @click="contact">Contact Page</button>
+    <button @click="service">Service Page</button>
+    <button @click="back">Back</button>
   </div>
 </template>
 
@@ -10,6 +14,17 @@ export default {
   data(){
     return{
 
+    }
+  },
+  methods:{
+    contact(){
+      this.$router.push('/contact');
+    },
+    service(){
+      this.$router.push('/service');
+    },
+    back(){
+      this.$router.go(-1);
     }
   }
 }
