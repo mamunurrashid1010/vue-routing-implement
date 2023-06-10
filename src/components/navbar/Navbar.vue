db745e438752c1969681efd88a36dba8abc96295
mamunurrashid1010 <mamunurrashid1010@gmail.com>
2023-06-10 10:45:25 +0600

    Navbar selected active link css added

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -26,4 +26,13 @@ export default {
   padding: 10px;
   font-size: 25px;
 }
+
+a.router-link-exact-active{
+  color: green;
+  background-color: #ffffff;
+  border: 1px solid gray;
+  border-radius: 10%;
+  padding: 5px;
+  font-weight: bold;
+}
 </style>
